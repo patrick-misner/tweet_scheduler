fd6173f5a31c78f7db995828960f2ddb1a25d013
Patrick Misner <misner.patrick@gmail.com>
2022-08-29 01:45:21 -0600

    tweet crud methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 
 class User < ApplicationRecord
   has_many :twitter_accounts
+  has_many :tweets
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid e-mail address" }
